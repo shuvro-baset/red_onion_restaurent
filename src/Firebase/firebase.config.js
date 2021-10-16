4ba5745c15c8b70a354a8c97ac698b2d3f28d921
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-16 15:54:43 +0600

    firebase initiailize

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDwiWteaB0aiHRqDq5xH6xARl7beAM8ivI",
+  authDomain: "red-onion-restaurant-5e643.firebaseapp.com",
+  projectId: "red-onion-restaurant-5e643",
+  storageBucket: "red-onion-restaurant-5e643.appspot.com",
+  messagingSenderId: "1072450295388",
+  appId: "1:1072450295388:web:efb926ef473a66260bf899"
+};
+
+export default firebaseConfig;
