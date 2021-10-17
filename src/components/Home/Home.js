2e1a5dcabe67e8f398e30c756b04edae5eb2d55a
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-17 09:37:12 +0600

    why choose us done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import useFoods from '../../hooks/useFoods';
 import FoodItem from '../Foods/FoodItem/FoodItem';
+import img1 from '../../Image/adult-blur-blurred-background-687824.png'
+import img2 from '../../Image/chef-cook-food-33614.png'
+import img3 from '../../Image/architecture-building-city-2047397.png'
+import abt_img1 from '../../Image/ICON/fast_delivery.png'
+import abt_img2 from '../../Image/ICON/notification.png'
+import abt_img3 from '../../Image/ICON/home_delivery.png'
 import './Home.css'
 const Home = () => {
 
@@ -36,6 +42,62 @@ const Home = () => {
                     {...food}></FoodItem>
                 )
             }
+            </Row>
+            <Row className="my-5">
+                <Col md={6}>
+                    <h2>Why You Choose Us</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Distinctio vel ad laborum unde officiis dolorem obcaecati possimus est, 
+                        nulla similique molestias
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={4}>
+                    <Card>
+                        <img src={img1} className="img-fluid" alt="" />
+                        <Row className='p-4'>
+                            <Col md={2}>
+                                <img src={abt_img1} className="abt_img" alt="" />
+                            </Col>
+                            <Col md={10}>
+                                <h3>Fast Delivery</h3>
+                                <p>Keep your system in a sync with automated web hook based notifications each time link is paid and how we dream about out future.</p>
+                                <button className="abt-btn">See more <i class="fas fa-arrow-circle-right"></i></button>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col md={4}>
+                    <Card>
+                        <img src={img2} className="img-fluid" alt="" />
+                        <Row className='p-4'>
+                            <Col md={2}>
+                                <img src={abt_img2} className="abt_img" alt="" />
+                            </Col>
+                            <Col md={10}>
+                                <h3>Fast Delivery</h3>
+                                <p>Keep your system in a sync with automated web hook based notifications each time link is paid and how we dream about out future.</p>
+                                <button className="abt-btn">See more <i class="fas fa-arrow-circle-right"></i></button>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col md={4}>
+                    <Card>
+                        <img src={img3} className="img-fluid" alt="" />
+                        <Row className='p-4'>
+                            <Col md={2}>
+                                <img src={abt_img3} className="abt_img" alt="" />
+                            </Col>
+                            <Col md={10}>
+                                <h3>Fast Delivery</h3>
+                                <p>Keep your system in a sync with automated web hook based notifications each time link is paid and how we dream about out future.</p>
+                                <button className="abt-btn">See more <i class="fas fa-arrow-circle-right"></i></button>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
             </Row>
         </Container>
     );
