7b8c31d3c709661037c2bb15509ad1d684033591
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-17 17:38:47 +0600

    registration

--- src/components/Foods/FoodItem/FoodItem.js
+++ src/components/Foods/FoodItem/FoodItem.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import './FoodItem.css'
 const FoodItem = ({ id, image, title, description, price , type}) => { 
     return (
         <Col md={4}>
-            <Card>
-                <img src={image} className="img-fluid" alt="" />
-                <h2>{title}</h2>
+            <Card className="p-3 shadow border-0 d-flex flex-column align-items-center">
+                <img src={image} className="img-fluid card-img" alt="" />
+                <h4>{title}</h4>
                 <p>{description.slice(0,100)}</p>
                 <p>{price}</p>
-                <button>{type}</button>
-                <Link to={`/food/${id}`}>click me</Link>
+                <button className="btn btn-danger"><Link to={`/food/${id}`} className="text-white">see details</Link></button>
             </Card>
         </Col>
     );
